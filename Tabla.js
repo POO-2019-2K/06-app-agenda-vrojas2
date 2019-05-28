de8bfe41464f61acc51be07b3d0161d318bc6ab8
Victor <vrojas2@ucol.mx>
2019-05-28 13:18:27 -0500

    borrarContacto

--- Tabla.js
+++ Tabla.js
@@ -9,14 +9,44 @@ export default class Tabla {
   }
 
   _initTables() {
-    let contactos = JSON.parse(localStorage.getItem("contactos"));
-    if(contactos === null){
+    let lsContactos = JSON.parse(localStorage.getItem("contactos"));
+    if(lsContactos === null){
       return;
     }
-    contactos.forEach((contacto, index) => {
-      contacto.birthday = new Date(contacto.birthday);
-      this._showInTable(new Contacto(contacto));
+    lsContactos.forEach((e, index) => {
+      e.birthday = new Date(e.birthday);
+      this._showInTable(new Contacto(e));
     });
+  }
+
+  _repetEmail(email){
+    let find = -1;
+    this._contactos.forEach((e, index) => {
+        if (e.email === email) {
+            find = index;
+            return;
+        }
+    });
+    return find;
+  }
+
+  addContacto(contacto) {
+    let found = this._repetEmail(contacto.email);
+    if (found >=0) {
+        swal.fire({
+            type: "error",
+            title: "Error",
+            text: "Este email ya ha sido utilizado"
+        });
+        return;
+    }
+    swal.fire({
+      type: 'success',
+      title: 'Registrado',
+      });
+    this._showInTable(contacto);
+      localStorage.setItem("contactos", JSON.stringify(this._contactos));
+      //console.log(localStorage.getItem("contactos"));
   }
 
   _showInTable(contacto) {
@@ -26,23 +56,38 @@ export default class Tabla {
     let cellEmail = row.insertCell(1);
     let cellBirthday = row.insertCell(2);
     let cellAge = row.insertCell(3);
+    row.insertCell(4);
 
     cellName.innerHTML = contacto.name;
     cellEmail.innerHTML = contacto.email;
     cellBirthday.innerHTML = contacto.getBirthdayAsString();
     cellAge.innerHTML = contacto.getAge();
 
-      let objContacto = {
-        name: contacto.name,
-        email: contacto.email,
-        birthday: contacto.birthday,
-      };
-      this._contactos.push(objContacto);
+    let objContacto = {
+      name: contacto.name,
+      email: contacto.email,
+      birthday: contacto.birthday,
+    };
+    
+    this._contactos.push(objContacto);
+    this._addButtons(row, contacto);
   }
 
-  addContacto(contacto) {
-    this._showInTable(contacto);
-      localStorage.setItem("contactos", JSON.stringify(this._contactos));
-      //console.log(localStorage.getItem("contactos"));
+  _deleteRow(row, contacto){
+    this._contactos.splice(contacto, 1);
+    row.innerHTML = ""; 
+    localStorage.setItem("Contacts", JSON.stringify(this._contactos));
+    return;  
+  }
+  
+  _addButtons(row, contacto){ 
+    let btnDelete = document.createElement("input");
+    btnDelete.type = "button";
+    btnDelete.value = "Borrar";
+    btnDelete.className = "btnDelet";
+    row.cells[4].appendChild(btnDelete);
+    btnDelete.addEventListener("click", () => { 
+      this._deleteRow(row, contacto);
+    }); 
   }
 }
